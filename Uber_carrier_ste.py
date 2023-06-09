f037eee1451ddb8c21e78c13ef3563118448d063
saurabh jain <65993641+jsourabh1@users.noreply.github.com>
2023-06-09 22:19:20 +0530

    Update Uber_carrier_ste.py

--- Uber_carrier_ste.py
+++ Uber_carrier_ste.py
@@ -1,7 +1,21 @@
 
+
 from bs4 import BeautifulSoup as beauty
 import requests
 import pandas as pd
+import json
+from pymongo import MongoClient
+from datetime import datetime
+
+client =MongoClient("mongodb://Saaurabh:Saurabh@ac-lhssquu-shard-00-00.yh1d1ea.mongodb.net:27017,ac-lhssquu-shard-00-01.yh1d1ea.mongodb.net:27017,ac-lhssquu-shard-00-02.yh1d1ea.mongodb.net:27017/JobHunter?ssl=true&replicaSet=atlas-tr39w0-shard-0&authSource=admin&retryWrites=true&w=majority")
+
+
+
+database = client["JobHunter"]
+
+my_collection = database["jobs"]
+
+
 
 header = {
     "x-csrf-token": "x",
@@ -12,7 +26,13 @@ url = requests.post(
 
 
 # content comes in the form of the Json so we don't make it's soup simple etracting the data/
-page_content_ = url.content
+page_content = url.content
+my_json = page_content.decode('utf8')
+print('- ' * 20)
+
+# Load the JSON to a Python list & dump it back out as formatted JSON
+page_content_ = json.loads(my_json)
+
 
 
 data = []
@@ -21,35 +41,50 @@ data = []
 # we are extracting  the team ,title and the location data from the json
 def get_content(index):
 
-    global data, page_content
-    content = page_content["data"]["results"][index]
+    global data, page_content_
+    content = page_content_["data"]["results"][index]
 
     #We are extracting the only the Engineering feild Jobs if you want another then you can look into the uber sites
 
-    if content["department"] == "Engineering":
-        job_title = content["title"]
-        job_id = content["id"]
-        job_url = "https://www.uber.com/global/en/careers/list/"+str(job_id)
+    job_department = content["department"]
+    job_title = content["title"]
+    job_id = content["id"]
+    job_url = "https://www.uber.com/global/en/careers/list/"+str(job_id)
 
 #         generated url: https://www.uber.com/global/en/carrers/list/111709/
 #         wanted url: https://www.uber.com/global/en/careers/list/111779/
 
-        #Preparing the data of the location
-        if len(content["allLocations"]) > 1:
-            job_location = "Multiple Locations"
+    #Preparing the data of the location
+    if len(content["allLocations"]) > 1:
+        city=set()
+        country=set()
+        for i in content["allLocations"]:
+          city.add(i["countryName"])
+          country.add(i["city"])
 
-        else:
-            job_location = content["location"]["city"] + \
-                content["location"]["region"]+content["location"]["country"]
+        job_city = list(city)
+        job_country = list(country)
+          
 
-        job = {
+    else:
+        job_city = [content["location"]["city"]]
+        job_country = [content["location"]["countryName"]]
 
-            "title": job_title,
-            "url": job_url,
-            "location": job_location
-        }
+    job = {
 
-        data.append(job)
+        "title": job_title,
+        "url": job_url,
+        "job_department":job_department,
+        "company":"uber",
+        "city":job_city,
+        "country":job_country,
+        "description": content["description"]
+      
+    }
+
+    data.append(job)
+    my_collection.insert_one(job)
+    print(len(data))
 
 
 # Going through all pages of the site
@@ -60,3 +95,5 @@ for i in range(1000):
 df = pd.DataFrame(data)
 df.to_csv("uber.csv")
 print("Program Done")
+
+
